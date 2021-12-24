6495ae09d51004f1ed436c6bad427eef177138da
Andrey Minin <ewkjf@mail.ru>
2021-12-24 12:23:16 +0300

    deleted failing lines of code

--- src/test/java/com/onlym/catalizator/CatalizatorApplicationTests.java
+++ src/test/java/com/onlym/catalizator/CatalizatorApplicationTests.java
@@ -2,11 +2,9 @@ package com.onlym.catalizator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class CatalizatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 }
