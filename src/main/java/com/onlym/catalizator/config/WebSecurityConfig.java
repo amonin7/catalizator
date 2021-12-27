bb3e53f955977768bd1641229d0fbbf94d0f74f4
Andrey Minin <ewkjf@mail.ru>
2021-12-27 18:11:01 +0300

    lesson4: added all parts, related to backend

--- src/main/java/com/onlym/catalizator/config/WebSecurityConfig.java
+++ src/main/java/com/onlym/catalizator/config/WebSecurityConfig.java
@@ -1,16 +1,26 @@
 package com.onlym.catalizator.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class WebSecurityConfig {
+
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
+
+    public WebSecurityConfig(AuthenticationManager authenticationManager, SecurityContextRepository securityContextRepository) {
+        this.authenticationManager = authenticationManager;
+        this.securityContextRepository = securityContextRepository;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -20,9 +30,25 @@ public class WebSecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
         return security
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
+                                )
+                )
+                .accessDeniedHandler(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
+                                )
+                )
+                .and()
                 .csrf().disable()
-                .formLogin().and()
+                .formLogin().disable()
                 .httpBasic().disable()
+                .authenticationManager(authenticationManager)
+                .securityContextRepository(securityContextRepository)
                 .authorizeExchange()
                 .pathMatchers("/", "/login", "/favicon.ico").permitAll()
                 .pathMatchers("/controller").hasRole("ADMIN")
